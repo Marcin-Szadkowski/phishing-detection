05e35f649416bb1c98372c28cc5605db3f6e3f94
Marcin Szadkowski <marcin.szadkowski@growbots.com>
2025-02-02 19:41:48 +0100

    Implemented VirusTotalClient based on vt lib.

--- src/phishing_detection/connectors/virus_total.py
+++ src/phishing_detection/connectors/virus_total.py
@@ -1,13 +1,26 @@
 import vt
 
 from phishing_detection import settings
+from phishing_detection.domain.models import VirusTotalUrlReport
 
 
 class VirusTotalClient:
     def __init__(self, api_key: str = settings.VIRUS_TOTAL_API_KEY):
-        self.api_key = api_key
-        self._client = vt.Client(api_key)
+        self._api_key = api_key
 
-    def get_url_report(self, url: str):
+    async def get_url_report(self, url: str) -> VirusTotalUrlReport:
+        """
+        https://docs.virustotal.com/reference/url-object
+
+        """
         url_id = vt.url_id(url)
-        return self._client.get_object(f"/urls/{url_id}")
+
+        report = await vt.Client(self._api_key).get_object_async(f"/urls/{url_id}")
+
+        return VirusTotalUrlReport(
+            harmless=report.last_analysis_stats["harmless"],
+            malicious=report.last_analysis_stats["malicious"],
+            suspicious=report.last_analysis_stats["suspicious"],
+            timeout=report.last_analysis_stats["timeout"],
+            undetected=report.last_analysis_stats["undetected"],
+        )
